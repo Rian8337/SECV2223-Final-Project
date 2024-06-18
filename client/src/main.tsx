9aee8b586f4256ffcc36aa4b07f86056ebd33183
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-06-18 15:01:31 +0800

    Remove unneeded `corner.css`

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeContextProvider } from "./hooks/ThemeContext.tsx";
 import { TodoContextProvider } from "./hooks/TodoContext.tsx";
-import "./styles/corner.css";
-import "./styles/index.css";
+import "./index.css";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
