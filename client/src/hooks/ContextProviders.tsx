6a5597b79841b294e593de3e11823550fb527e76
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-06-18 15:03:35 +0800

    Consolidate context providers into one `Providers` element

---
+++ client/src/hooks/ContextProviders.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { TodoContextProvider } from "./TodoContext";
+import { ThemeContextProvider } from "./ThemeContext";
+
+const compose =
+    (...components: FC<Record<string, unknown>>[]) =>
+    (props: { children: JSX.Element }) =>
+        components.reduce(
+            (children, Current) => <Current {...props}>{children}</Current>,
+            props.children
+        );
+
+export const Providers = compose(TodoContextProvider, ThemeContextProvider);
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ThemeContextProvider } from "./hooks/ThemeContext.tsx";
-import { TodoContextProvider } from "./hooks/TodoContext.tsx";
 import "./index.css";
+import { Providers } from "./hooks/ContextProviders.tsx";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <TodoContextProvider>
-        <ThemeContextProvider>
-            <React.StrictMode>
-                <App />
-            </React.StrictMode>
-        </ThemeContextProvider>
-    </TodoContextProvider>
+    <Providers>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Providers>
 );
