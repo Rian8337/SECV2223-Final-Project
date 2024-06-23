d81eca4a7c86e42468681ecc3dc48bd77c68410f
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-06-23 11:40:15 +0800

    Add database method to fully retrieve a family's information

--- server/db/Db.php
+++ server/db/Db.php
@@ -55,6 +55,58 @@ class Db
         return $this->db;
     }
 
+    /**
+     * Get the full family details.
+     * 
+     * @param int $familyId The ID of the family.
+     * @return array|null The family details or `null` if the family does not exist.
+     */
+    public function getFullFamily(int $familyId): array|null
+    {
+        // Obtain family
+        $family = $this->query(
+            sprintf(
+                "SELECT * FROM %s WHERE id = %d",
+                Db::family_table,
+                $familyId
+            )
+        )->fetch_assoc();
+
+        if (!$family) {
+            return null;
+        }
+
+        // Populate family members
+        $family["members"] = $this->query(
+            sprintf(
+                "SELECT id, name, email FROM %s WHERE family_id = %d",
+                Db::user_table,
+                $familyId
+            )
+        )->fetch_all(MYSQLI_ASSOC);
+
+        // Get family member roles
+        $familyMemberRoles = $this->query(
+            sprintf(
+                "SELECT user_id, role FROM %s WHERE family_id = %d",
+                Db::family_member_table,
+                $familyId
+            )
+        )->fetch_all(MYSQLI_ASSOC);
+
+        // Assign roles to family members
+        foreach ($family["members"] as &$member) {
+            foreach ($familyMemberRoles as $role) {
+                if ($role["user_id"] === $member["id"]) {
+                    $member["role"] = $role["role"];
+                    break;
+                }
+            }
+        }
+
+        return $family;
+    }
+
     public function __destruct()
     {
         $this->db->close();
