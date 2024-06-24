d5778e5ee72e1b62c0db58d76e1b834918ffffe3
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-06-24 09:09:37 +0800

    Allow OPTIONS request method for preflight requests

--- server/core/Env.php
+++ server/core/Env.php
@@ -1,6 +1,12 @@
 <?php
 
 header('Access-Control-Allow-Origin: http://localhost:5173');
-header('Access-Control-Allow-Methods: GET, POST, DELETE, PATCH, PUT');
+header('Access-Control-Allow-Methods: GET, POST, DELETE, PATCH, PUT, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 header('Access-Control-Allow-Credentials: true');
+
+// Return OPTIONS request immediately
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
