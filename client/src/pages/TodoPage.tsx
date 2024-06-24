38e4ae7dccd31c38840fee9a133688a770205adb
Rian (Reza Mouna Hendrian) <52914632+Rian8337@users.noreply.github.com>
2024-06-24 18:49:30 +0800

    Prevent TODO page from being loaded if user is not logged in

--- client/src/pages/TodoPage.tsx
+++ client/src/pages/TodoPage.tsx
@@ -6,13 +6,24 @@ import PageWrapper from "../components/PageWrapper";
 import AddTodo from "../components/todo/AddTodo";
 import { ThemeContext } from "../hooks/ThemeContext";
 import "./TodoPage.css";
+import { UserContext } from "../hooks/UserContext";
+import { useNavigate } from "react-router-dom";
 
 export default function TodoPage() {
+    const navigate = useNavigate();
+
+    const userCtx = useContext(UserContext);
     const themeCtx = useContext(ThemeContext);
     const todoCtx = useContext(TodoContext);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
+        if (userCtx.value === null) {
+            navigate("/login");
+
+            return;
+        }
+
         // Get existing todos from the server
         TodoService.getTodos()
             .then((data) => {
@@ -29,7 +40,7 @@ export default function TodoPage() {
             });
         // We only want to run this hook once.
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [userCtx.value]);
 
     if (error !== null) {
         return (
